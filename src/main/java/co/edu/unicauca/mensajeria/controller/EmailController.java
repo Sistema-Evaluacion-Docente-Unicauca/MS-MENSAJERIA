c42bf181ec3bf15b84b714b9db9fcfcb2c85a493
jhayber17 <jhayberm@unicauca.edu.co>
2025-05-01 09:25:17 -0500

    Feature: add dockerfile

--- src/main/java/co/edu/unicauca/mensajeria/controller/EmailController.java
+++ src/main/java/co/edu/unicauca/mensajeria/controller/EmailController.java
@@ -1,26 +1,53 @@
 package co.edu.unicauca.mensajeria.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import co.edu.unicauca.mensajeria.dto.EmailRequest;
 import co.edu.unicauca.mensajeria.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/mensajeria")
+@RequiredArgsConstructor
+@Tag(name = "Mensajería", description = "Servicio para envío de correos electrónicos con documentos adjuntos")
 public class EmailController {
 
     private static final Logger logger = LoggerFactory.getLogger(EmailController.class);
-
-    @Autowired
-    private EmailService emailService;
+    private final EmailService emailService;
 
     @PostMapping("enviar-email")
+    @Operation(
+        summary = "Enviar correo electrónico con anexos",
+        description = "Permite enviar un correo electrónico a múltiples destinatarios, incluyendo documentos adjuntos.",
+        requestBody = @RequestBody(
+            required = true,
+            description = "Datos del correo a enviar",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = EmailRequest.class)
+            )
+        ),
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Correo enviado exitosamente",
+                content = @Content(schema = @Schema(implementation = Boolean.class))
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                description = "Error al enviar el correo",
+                content = @Content(schema = @Schema(implementation = Boolean.class))
+            )
+        }
+    )
     public ResponseEntity<Boolean> enviarCorreoConAnexos(@RequestBody EmailRequest request) {
         logger.info("Recibiendo solicitud para enviar correo a: {}", request.getCorreos());
 
